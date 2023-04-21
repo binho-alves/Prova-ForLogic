b0dbf5bed54d3d57fa6499415549bcb2bc9b183c
mzt.fralves <mzt.fralves@voegol.com.br>
2023-04-21 12:25:12 -0300

    Adicionando arquivos do projeto

---
+++ Template.Domain/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Template.Domain
+{
+    class User
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
